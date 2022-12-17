fdfa91681f7f8a34d82c6ab51dd20cae964c8874
Marvin <marvk3@gmail.com>
2022-12-17 11:11:53 +0100

    17 cleaned up

--- src/day17.rs
+++ src/day17.rs
@@ -28,113 +28,84 @@ pub struct Part2;
 
 impl Part<u64> for Part2 {
     fn expect_test(&self) -> u64 {
-        1514285714288
+        1_514_285_714_288
     }
 
     fn solve(&self, input: &Vec<String>) -> u64 {
         let vec = parse_input(input);
-        play(vec, 1000000000000)
+        play(vec, 1_000_000_000_000)
     }
 }
 
 fn play(directions: Vec<Point>, steps: u64) -> u64 {
-    let mut direction_index = 0;
-    let mut next_direction = move || {
-        let result = directions[direction_index].clone();
-        direction_index = (direction_index + 1) % directions.len();
-        result
+    let mut next_direction = {
+        let mut direction_index = 0;
+        move || {
+            let result = directions[direction_index].clone();
+            direction_index = (direction_index + 1) % directions.len();
+            result
+        }
     };
 
     let rocks = vec![RockShape::a(), RockShape::b(), RockShape::c(), RockShape::d(), RockShape::e()];
-    let occupied_per_rock_cycle: u32 = rocks.iter().map(|r| r.points.len() as u32).sum();
-    let n_rocks = rocks.len();
-    let mut rock_index = 0;
-    let mut next_rock = move || {
-        let result = rocks[rock_index].clone();
-        rock_index = (rock_index + 1) % rocks.len();
-        result
+    let occupied_per_rock_cycle = rocks.iter().map(|r| r.points.len()).sum::<usize>() as u64;
+    let n_rocks = rocks.len() as u64;
+    let mut next_rock = {
+        let mut rock_index = 0;
+        move || {
+            let result = rocks[rock_index].clone();
+            rock_index = (rock_index + 1) % rocks.len();
+            result
+        }
     };
 
     let map = Map::new();
 
-    let mut step = |search_cycles: bool| -> Option<usize> {
-        if search_cycles {
-            if let Some(cycle) = map.detect_cycle() {
-                return Some(cycle);
-            }
-        }
-
+    let mut step = || {
         let rock = Rock::new(map.insertion_origin(), next_rock());
 
         loop {
             let direction = next_direction();
-            let can_be_pushed = rock.shape.points.iter().map(|p| *p + *rock.position.borrow() + direction).all(|p| {
-                !map.is_occupied(&p)
-            });
-            if can_be_pushed {
-                rock.translate(direction);
+            if map.can_accommodate_rock_with_offset(&rock, &direction) {
+                rock.translate(&direction);
             }
 
-            let can_fall = rock.shape.points.iter().map(|p| *p + *rock.position.borrow() + Point::DOWN).all(|p| {
-                !map.is_occupied(&p)
-            });
-            if can_fall {
-                rock.translate(Point::DOWN);
+            if map.can_accommodate_rock_with_offset(&rock, &Point::DOWN) {
+                rock.translate(&Point::DOWN);
             } else {
-                rock.shape.points.iter().map(|p| *p + *rock.position.borrow()).for_each(|p| {
-                    map.insert(p);
-                });
-                break;
+                map.insert_rock(rock);
+                return;
             }
         }
-
-        None
     };
-
 
     let maybe_cycle =
         (0..steps)
-            .find_map(|s| step(true).map(|cycle_len| (s, cycle_len)));
+            .find_map(|s| {
+                if let Some(cycle) = map.find_cycle() {
+                    Some((s, cycle))
+                } else {
+                    step();
+                    None
+                }
+            });
 
-    if let Some((n_steps, cycle_len)) = maybe_cycle {
-        let remaining_steps = steps - n_steps;
+    if let Some((steps_taken, cycle_height)) = maybe_cycle {
+        let occupied_in_last_rows = map.count_occupied_in_last_rows(cycle_height);
+        let cycle_len = n_rocks * occupied_in_last_rows / occupied_per_rock_cycle;
 
-        let occupied_in_last_rows = map.count_occupied_in_last_rows(cycle_len);
-        let cycle_len = ((occupied_in_last_rows / occupied_per_rock_cycle) as u64) * n_rocks as u64;
+        let remaining_steps = steps - steps_taken;
+        let residual_steps = remaining_steps % cycle_len;
+        let remaining_cycles = remaining_steps / cycle_len;
 
-        let residue = remaining_steps % cycle_len;
+        for _ in 0..residual_steps { step() }
 
-        for _ in 0..residue {
-            step(false);
-        }
+        let y_per_cycle = cycle_height as u64;
 
-        let y_before_cycle = *map.min_y.borrow();
-
-        for _ in 0..cycle_len {
-            step(false);
-        }
-
-        let y_after_cycle = *map.min_y.borrow();
-
-        let y_per_cycle = y_after_cycle - y_before_cycle;
-
-
-        let i = remaining_steps - residue;
-        let remaining_cycles = ((i) / cycle_len) as i64;
-        // println!(" occupied_in_last_rows     {}", occupied_in_last_rows);
-        // println!(" occupied_per_rock_cycle   {}", occupied_per_rock_cycle);
-        // println!(" steps_before_cycle_found  {}", n_steps);
-        // println!(" cycle_len                 {}", cycle_len);
-        // println!(" residue                   {}", residue);
-        // println!(" y_before_cycle            {}", y_before_cycle);
-        // println!(" y_after_cycle             {}", y_after_cycle);
-        // println!(" remaining_cycles          {}", remaining_cycles);
-        // println!(" y_per_cycle               {}", y_per_cycle);
-
-        1 - (y_before_cycle + remaining_cycles * y_per_cycle) as u64
+        remaining_cycles * y_per_cycle + map.height()
     } else {
-        let r = *map.min_y.borrow();
-        (1 - (r)) as u64
+        // No cycle found, just calculate the result
+        map.height()
     }
 }
 
@@ -163,53 +134,70 @@ impl Map {
     }
 
     fn is_occupied(&self, p: &Point) -> bool {
-        p.x < 0 || p.x > 6 || p.y > 0 || self.raw.borrow().get(-p.y as usize).map(|u| u & (1 << (p.x as u8)) != 0).unwrap_or(false)
+        p.x < 0 || p.x > 6 || p.y > 0 || self.is_raw_occupied(p)
+    }
+
+    fn is_raw_occupied(&self, p: &Point) -> bool {
+        self.raw.borrow().get(-p.y as usize)
+            .map(|u| u & (1 << (p.x)) != 0)
+            .unwrap_or(false)
+    }
+
+    fn can_accommodate_rock_with_offset(&self, rock: &Rock, offset: &Point) -> bool {
+        rock.shape.points.iter()
+            .map(|p| *p + *rock.position.borrow() + *offset)
+            .all(|p| !self.is_occupied(&p))
     }
 
     fn insertion_origin(&self) -> Point {
         p(2, *self.min_y.borrow() - 4)
     }
 
+    fn insert_rock(&self, rock: Rock) {
+        rock.shape.points.iter()
+            .map(|p| *p + *rock.position.borrow())
+            .for_each(|p| self.insert(p));
+    }
+
     fn insert(&self, p: Point) {
         let new_min_y = min(*self.min_y.borrow(), p.y);
 
-        for _ in 0..-(new_min_y - *self.min_y.borrow()) {
-            self.raw.borrow_mut().push(0);
-        }
+        self.insert_blank_rows(*self.min_y.borrow() - new_min_y);
+        self.insert_raw(p);
 
         self.min_y.replace(new_min_y);
-
-        let i = &mut self.raw.borrow_mut()[-p.y as usize];
-        *i = *i | (1 << p.x)
     }
 
-    fn count_occupied_in_last_rows(&self, n: usize) -> u32 {
+    fn insert_blank_rows(&self, n: i64) {
+        for _ in 0..n {
+            self.raw.borrow_mut().push(0);
+        }
+    }
+
+    fn insert_raw(&self, p: Point) {
+        let row = &mut self.raw.borrow_mut()[-p.y as usize];
+        *row = *row | (1 << p.x)
+    }
+
+    fn count_occupied_in_last_rows(&self, n: usize) -> u64 {
         self.raw.borrow().iter()
             .rev()
             .take(n)
-            .map(|u| u.count_ones())
+            .map(|u| u.count_ones() as u64)
             .sum()
     }
 
-    fn detect_cycle(&self) -> Option<usize> {
+    fn find_cycle(&self) -> Option<usize> {
         let arr = self.raw.borrow();
         let y0 = arr.len() - 1;
 
-        if arr.len() < 100 {
-            return None;
-        }
-
-        'outer: for cycle_length in (5..(arr.len() / 4)).rev() {
+        'outer: for cycle_length in (5..(y0 / 4)).rev() {
             for i in 0..cycle_length {
-                let i1 = arr[y0 - i];
-                let i2 = arr[y0 - i - cycle_length];
-                let i3 = arr[y0 - i - cycle_length * 2];
-                // println!();
-                // println!("{}", arr.len());
-                // println!("{}", cycle_length);
-                let i4 = arr[y0 - i - cycle_length * 3];
+                let i1 = arr.get(y0 - i)?;
+                let i2 = arr.get(y0 - i - cycle_length)?;
+                let i3 = arr.get(y0 - i - cycle_length * 2)?;
 
-                if i1 != i2 || i2 != i3 || i3 != i4 {
+                if i1 != i2 || i2 != i3 {
                     continue 'outer;
                 }
             }
@@ -218,6 +206,10 @@ impl Map {
         }
 
         None
+    }
+
+    fn height(&self) -> u64 {
+        1 - *self.min_y.borrow() as u64
     }
 }
 
@@ -238,7 +230,6 @@ impl Debug for Map {
         }
 
         result.push_str("+-------+");
-
 
         write!(f, "{}", result)
     }
@@ -287,8 +278,8 @@ impl Rock {
         self.position.borrow().x + self.width() - 1
     }
 
-    fn translate(&self, d: Point) {
-        let new_position = *self.position.borrow() + d;
+    fn translate(&self, d: &Point) {
+        let new_position = *self.position.borrow() + *d;
         self.position.replace(new_position);
     }
 }
